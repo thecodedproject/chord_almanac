feb48cc7aa51a4a6979a6445d0bb1508be2510d6
Jonny Cooper <jdc.tas@gmail.com>
2026-06-08 12:31:10 +0100

    setup index page routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,23 @@
-import "./App.css"
+import { NavLink, Route, Routes } from "react-router-dom";
+import "./App.css";
 
-import {
-  GuitarTetradInversions
-} from './pages/guitar_tetrad_inversions'
+import { ChordCyclePage } from './pages/chord_cycle_page';
+import { GuitarScalePage } from './pages/guitar_scale_page';
+import { GuitarTetradInversions } from './pages/guitar_tetrad_inversions';
 
-
-export default function App() {
-
+export default function App(): JSX.Element {
   return (
     <>
-      <GuitarTetradInversions />
+      <nav className="appNav">
+        <NavLink to="/" end>Guitar Scale</NavLink>
+        <NavLink to="/tetrad-inversions">Tetrad Inversions</NavLink>
+        <NavLink to="/chord-cycle">Chord Cycle</NavLink>
+      </nav>
+      <Routes>
+        <Route path="/" element={<GuitarScalePage />} />
+        <Route path="/tetrad-inversions" element={<GuitarTetradInversions />} />
+        <Route path="/chord-cycle" element={<ChordCyclePage />} />
+      </Routes>
     </>
-  )
+  );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { HashRouter } from "react-router-dom";
 import "./styles.css";
 
 import App from "./App";
@@ -8,6 +9,8 @@ import App from "./App";
 const root = createRoot(document.getElementById("root") as HTMLElement);
 root.render(
   <StrictMode>
-    <App />
+    <HashRouter>
+      <App />
+    </HashRouter>
   </StrictMode>
 );
